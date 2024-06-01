36b25a11bdc78647006aeffadef2c4812ca2e483
Sander Bollen <sander@sndrtj.eu>
2024-06-01 15:54:22 +0200

    Update headers

--- src/droombot/models.py
+++ src/droombot/models.py
@@ -1,4 +1,4 @@
-#    Copyright 2023 Sander Bollen
+#    Copyright 2023-2024 Sander Bollen
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
