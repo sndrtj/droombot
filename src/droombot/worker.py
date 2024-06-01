aff2ab97d4515bf7f8061dc24ef29064c76b3f60
Sander Bollen <sander@sndrtj.eu>
2024-06-01 14:58:23 +0200

    Some fixes in worker

--- src/droombot/worker.py
+++ src/droombot/worker.py
@@ -12,10 +12,10 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-
 import asyncio
 import json
 import logging
+import traceback
 
 import aiohttp
 import pydantic
@@ -34,9 +34,20 @@ class Worker:
     def __init__(self):
         self._redis_connection = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
+        self._running_tasks: set[asyncio.Task] = set()
+        self._wait_lock = asyncio.Lock()
+
     def run(self):
         logger.info("Starting worker...")
         asyncio.run(self.loop())
+
+    def _done_callback(self, t: asyncio.Task) -> None:
+        if (exc := t.exception()) is not None:
+            logger.error(
+                f"Task {t.get_name()} raised an exception during execution...: {exc} "
+                f"\n \n {''.join(traceback.format_tb(exc.__traceback__))}"
+            )
+        self._running_tasks.discard(t)
 
     async def loop(self):
         """Main application loop"""
@@ -46,7 +57,9 @@ class Worker:
             await pubsub.subscribe("droombot-prompts")
             while True:
                 async with limiter:
-                    asyncio.ensure_future(self.read_and_run(pubsub, http_client))
+                    task = asyncio.create_task(self.read_and_run(pubsub, http_client))
+                    self._running_tasks.add(task)
+                    task.add_done_callback(self._done_callback)
 
     async def read_and_run(
         self, channel: redis.client.PubSub, session: aiohttp.ClientSession
@@ -57,7 +70,9 @@ class Worker:
 
         :return: None
         """
-        message = await channel.get_message(ignore_subscribe_messages=True)
+        async with self._wait_lock:
+            message = await channel.get_message(ignore_subscribe_messages=True)
+
         if message is None:
             logger.debug("No message to process...")
             return
@@ -74,7 +89,7 @@ class Worker:
             f"'{deserialized_message.text_prompt}'"
         )
 
-        text_to_image_request = pubsub_to_t2i(message)
+        text_to_image_request = pubsub_to_t2i(deserialized_message)
 
         logger.info("Running text-to-image conversion")
         responses = await text_to_image(session, text_to_image_request)
